613f689ea4ccb7288c4b028f7dc3fbc7fbfde2b6
thegabbythemaster <gabby@DESKTOP-TOMMG36.localdomain>
2020-01-01 18:38:12 -0500

    Tropical files inherits from climate

---
+++ Tropical.cpp
@@ -0,0 +1,41 @@
+#include "Climate.hpp"
+#include "Tropical.hpp"
+#include <string>
+#include <iostream>
+
+Tropical::Tropical():name_(""), type_(""), humidity_(false), temperature_(""), air_pressure_(""){}
+
+Tropical::Tropical(std::string name, std::string type, bool humidity, std::string temperature, std::string air_pressure):
+Climate(name,type,humidity,temperature,air_pressure),TropicalHumid_(false),Trees_(0),Species_(0),Rain_(false){}
+
+void Tropical::setTropicalHumidity(){
+	TropicalHumid_ = true;
+} 
+
+bool Tropical::TropicalIsHumid(){
+	return TropicalHumid_;
+}
+
+void Tropical::setTropicalTrees(int numberOfTropicalTrees){
+	Trees_ = numberOfTropicalTrees;
+}
+
+int Tropical::numberOfTropicalTrees(){
+	return Trees_;
+}
+
+void Tropical::setTropicalRain(){
+	Rain_ = true;
+}
+
+bool Tropical::hasRain(){
+	return Rain_;
+}
+
+void Tropical::setNumberOfSpecies(int NumberOfSpecies){
+	Species_ = NumberOfSpecies;
+}
+
+int Tropical::NumberOfSpecies(){
+	return Species_;
+}
---
+++ Tropical.hpp
@@ -0,0 +1,47 @@
+/*tropical HPP file created by Gabriella Gonzalez
+inherits from the class CLimate
+*/
+#ifndef TROPICAL_H_
+#define TROPICAL_H_
+
+#include "Climate.hpp"
+#include <iostream>
+
+class Tropical: public Climate {
+	public:
+		Tropical() = default;
+        Tropical(std::string name, std::string type, bool humidity, std::string temperature, std::string air_pressure);
+
+        //sets the tropical humidity to true
+		void setTropicalHumidity();
+
+		//@return whether the tropical climate it hot or not
+		bool TropicalIsHumid();
+
+		void setTropicalTrees(int numberOfTropicalTrees);
+
+		//returns the number of trees in tropical
+		int numberOfTropicalTrees();
+
+		//sets the tropical rain to true
+		void setTropicalRain();
+
+		//@return if tropical has rain or not
+		bool hasRain();
+
+		//sets the number of species living
+		void setNumberOfSpecies(int NumberOfSpecies);
+		
+		//returns the number of species living
+		int NumberOfSpecies();
+
+		virtual void display();
+
+	private:
+		bool TropicalHumid_; //if tropical is humid or not
+		int Trees_; //has trees or not
+		int Species_; //# of species 
+		bool Rain_; //is raining or not
+
+
+}//end of Tropical.hpp
