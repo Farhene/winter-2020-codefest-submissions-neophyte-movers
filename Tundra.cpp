14e4b5a50b7a9de4826d6fa02d1e3b1b71d82cf3
Farhene Sultana <farhenesultana16@Farhenes-MBP.home>
2020-01-09 21:15:56 -0500

    Edited display functions of the climates

--- Tundra.cpp
+++ Tundra.cpp
@@ -49,10 +49,34 @@ bool Tundra::hasMountains()
 {   return mountains_;  }
 
 
-
-/*******************************************************************************/
-/********************************* NOT DONE YET ********************************/
-/*******************************************************************************/
-
 //display info
-void Tundra::display(){}
+void Tundra::display()
+{
+   std::cout << "The Tundra climate tend to have mountains.\n
+   It is treeless and soil is permanently frozen.
+   \nAn example of a Tundra is the Arctic and on top of mountains where
+   climate is cold and windy. 
+   \nSummers here bring bursts of wildflowers however!" << std::endl;
+   std::cout << "Please type 'Summer' or 'Winter' to see an example of Tundra during the Summer or Winter"
+   std::string result = "";
+   std::cin >> result;
+   while(result != "Summer" && result != "summer" && result != "Winter" && result != "winter")
+   {
+           std::cout << "Please type either 'Summer' or 'Winter'."
+   }
+   if(result == "Summer" || result == "summer")
+   {
+        std::cout << "Here is a link to copy and search on Google to see an example of a Tundra SUMMER climate:
+        \nhttps://www.conserve-energy-future.com/wp-content/uploads/2019/03/alpine-tundra-daylight-environment.jpg" << std::endl;
+        std::cout << "And here is the WINTER link!
+        \nhttps://s3.amazonaws.com/user-media.venngage.com/864469-0deee697ae5d1297d949a4698e625002.jpeg" << std::endl;
+   }
+   else
+   {
+        std::cout << "Here is a link to copy and search on Google to see an example of a Tundra WINTER climate:
+        \nhttps://s3.amazonaws.com/user-media.venngage.com/864469-0deee697ae5d1297d949a4698e625002.jpeg" << std::endl;
+        std::cout << "And here is the SUMMER link!
+        \nhttps://www.conserve-energy-future.com/wp-content/uploads/2019/03/alpine-tundra-daylight-environment.jpg" << std::endl;
+   }
+   
+}
