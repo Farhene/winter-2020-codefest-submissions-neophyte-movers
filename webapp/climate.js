1f925f2ba9f9884fbfa2051ae5a931d36542b54d
Gabriella Alexis <Gabby@Gabbys-Mac.local>
2020-01-13 21:48:15 -0500

    updated look on main page and overall + 3 questions for energy types

--- webapp/climate.js
+++ webapp/climate.js
@@ -2,7 +2,7 @@ const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-openModalButtons.forEach(a => {
+openModalButtons.forEach(a => { 
   a.addEventListener('click', () => {
     const modal = document.querySelector(a.dataset.modalTarget)
     openModal(modal)
