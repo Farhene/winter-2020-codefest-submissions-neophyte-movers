6c8830f7e628fc2d61ef2eba37171afd72a07b13
Farhene Sultana <farhenesultana16@Farhenes-MacBook-Pro.local>
2020-01-15 15:03:04 -0500

    proceeding with the web

--- C++/climatechange.cpp
+++
@@ -1,6 +0,0 @@
-function display(){
-    alert("You will first be given a series of questions. Next you will be given a Climate to explore under and read through what is special about each climate and how easy it is to protect our planet.");
-}
-function play(){
-    <a href="">here</a>
-}
